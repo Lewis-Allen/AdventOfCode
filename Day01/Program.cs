fb1d93974d491856dd98190359dc36b5622bb47e
Lewis-Allen <lewisn11@hotmail.co.uk>
2020-12-01 17:59:50 +0000

    Add project files.

---
+++ Day01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var integers = File.ReadAllLines("../../../input.txt").Select(val => int.Parse(val)).ToList();
+
+            for(var i = 0; i < integers.Count; i++)
+            {
+                for(var j = i + 1; j < integers.Count; j++)
+                {
+                    for(var k = j + 1; k < integers.Count; k++)
+                    {
+                        if (integers[i] + integers[j] + integers[k] == 2020)
+                            Console.WriteLine(integers[i] * integers[j] * integers[k]);
+                    }
+                }
+            }
+        }
+    }
+}
