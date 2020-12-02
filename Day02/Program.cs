a3bb3c408ef8d53ae2b6eedf52b5cf314359a20b
Lewis-Allen <lewisn11@hotmail.co.uk>
2020-12-02 18:21:16 +0000

    Linqy Linq

--- Day02/Program.cs
+++ Day02/Program.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 
-var lines = File.ReadAllLines("../../../input.txt");
-
-int validPasswordCount = 0;
-foreach(var line in lines)
+int validPasswordCount = File.ReadAllLines("../../../input.txt").Count(line =>
 {
-    string[] values = line.Split(new[] { "-", " " }, StringSplitOptions.RemoveEmptyEntries);
+    string[] values = line.Split(new[] { "-", " " }, StringSplitOptions.None);
 
     int min = int.Parse(values[0]) - 1;
     int max = int.Parse(values[1]) - 1;
@@ -14,8 +12,7 @@ foreach(var line in lines)
     char letter = values[2][0];
     string password = values[3];
 
-    if (password[min] == letter ^ password[max] == letter)
-        validPasswordCount++;
-}
+    return password[min] == letter ^ password[max] == letter;
+});
 
 Console.WriteLine(validPasswordCount);
