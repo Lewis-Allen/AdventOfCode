bba440cda161c0f5605a28b255b9e17279b22d5e
Lewis-Allen <lewisn11@hotmail.co.uk>
2020-12-03 18:36:21 +0000

    Day 03

--- Day02/Program.cs
+++ Day02/Program.cs
@@ -2,7 +2,29 @@
 using System.IO;
 using System.Linq;
 
-int validPasswordCount = File.ReadAllLines("../../../input.txt").Count(line =>
+var lines = File.ReadAllLines("../../../input.txt");
+
+int validPasswordCount = lines.Count(PartOneVerify);
+Console.WriteLine($"Part one valid passwords: {validPasswordCount}");
+
+validPasswordCount = lines.Count(PartTwoVerify);
+Console.WriteLine($"Part two valid passwords: {validPasswordCount}");
+
+static bool PartOneVerify(string line)
+{
+    string[] values = line.Split(new[] { "-", " " }, StringSplitOptions.None);
+
+    int min = int.Parse(values[0]) - 1;
+    int max = int.Parse(values[1]) - 1;
+
+    char letter = values[2][0];
+    string password = values[3];
+
+    int occurrences = password.Count(c => c == letter);
+    return occurrences >= min && occurrences <= max;
+}
+
+static bool PartTwoVerify(string line)
 {
     string[] values = line.Split(new[] { "-", " " }, StringSplitOptions.None);
 
@@ -13,6 +35,5 @@ int validPasswordCount = File.ReadAllLines("../../../input.txt").Count(line =>
     string password = values[3];
 
     return password[min] == letter ^ password[max] == letter;
-});
+}
 
-Console.WriteLine(validPasswordCount);
---
+++ Day03/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+var lines = File.ReadAllLines("../../../input.txt");
+
+var slope = string.Join("", lines.Select(s => s.Trim()).ToArray());
+
+int length = lines[0].Length;
+int totalTrees = CheckSlope(slope, length, 1, 1) *
+                 CheckSlope(slope, length, 3, 1) *
+                 CheckSlope(slope, length, 5, 1) *
+                 CheckSlope(slope, length, 7, 1) *
+                 CheckSlope(slope, length, 1, 2);
+
+
+Console.WriteLine(totalTrees);
+
+static int CheckSlope(string slope, int length, int x, int y)
+{
+    int noOfTrees = 0;
+    int curPos = 0;
+
+    while (curPos < slope.Length)
+    {
+        if (slope[curPos] == '#')
+            noOfTrees++;
+
+        curPos = ((curPos + x) / length > curPos / length ? curPos - (curPos % length) + ((curPos + x) % length) : curPos + x) + (y * length);
+    }
+
+    return noOfTrees;
+}
