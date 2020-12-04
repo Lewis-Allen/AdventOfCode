42c29a79fe85c6697019858a6828440c98cf4d80
Lewis-Allen <lewisn11@hotmail.co.uk>
2020-12-04 21:14:34 +0000

    Refactor

--- Day04/Passport.cs
+++ Day04/Passport.cs
@@ -18,65 +18,17 @@ namespace Day04
         public string EyeColour { get; set; }
         public string PassportID { get; set; }
         public string CountryID { get; set; }
-        public string ErrorMessage { get; set; }
 
         public bool IsValid()
         {
             return
-                ValidateYear(BirthYear, 4, 1920, 2002) && 
-                ValidateYear(IssueYear, 4, 2010, 2020) &&
-                ValidateYear(ExpirationYear, 4, 2020, 2030) &&
-                ValidateHeight(Height) &&
-                ValidateHair(HairColour) &&
-                ValidateEyeColour(EyeColour) &&
-                ValidatePassportID(PassportID);
+                int.TryParse(BirthYear, out int birthYear) && birthYear >= 1920 && birthYear <= 2002 &&
+                int.TryParse(IssueYear, out int issueYear) && issueYear >= 2010 && issueYear <= 2020 &&
+                int.TryParse(ExpirationYear, out int expirationYear) && expirationYear >= 2020 && expirationYear <= 2030 &&
+                Regex.IsMatch(Height ?? "", "(1(([5-8][0-9])|(9[0-3]))cm)|(((59)|(6[0-9])|(7[0-6]))in)") &&
+                Regex.IsMatch(HairColour ?? "", "#[\\da-f]{6}") &&
+                Regex.IsMatch(EyeColour ?? "", "amb|blu|brn|gry|grn|hzl|oth") &&
+                Regex.IsMatch(PassportID ?? "", "^[\\d]{9}$");
         }
-
-        public bool ValidateYear(string value, int length, int min, int max)
-        {
-            if (string.IsNullOrEmpty(value)) return false;
-            if (!(length == 0 || value.Length == length)) return false;
-
-            if (int.TryParse(value, out int year))
-            {
-                return year >= min && year <= max;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        public bool ValidateHeight(string value)
-        {
-            if (string.IsNullOrEmpty(value)) return false;
-            if (!int.TryParse(Regex.Match(value, @"\d+").Value, out int number)) return false;
-
-            if (value.Contains("cm"))
-            {
-                return number >= 150 && number <= 193;
-            }
-            else if (value.Contains("in"))
-            {
-                return number >= 59 && number <= 76;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        public bool ValidateHair(string value) => (!string.IsNullOrEmpty(value) && Regex.Match(value, "#[\\da-f]{6}").Success);
-
-        public bool ValidateEyeColour(string value) => !string.IsNullOrEmpty(value) &&
-                                                        (value == "amb" ||
-                                                        value == "blu" ||
-                                                        value == "brn" ||
-                                                        value == "gry" ||
-                                                        value == "grn" ||
-                                                        value == "hzl" ||
-                                                        value == "oth");
-
-        public bool ValidatePassportID(string value) => (!string.IsNullOrEmpty(value) && value.Length == 9 && Regex.Match(value, "[\\d]{9}").Success);
     }
 }
