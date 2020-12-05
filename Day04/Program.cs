06f01c1eed81018b9a6fa1fd7720ff96c8491ad2
Lewis-Allen <lewisn11@hotmail.co.uk>
2020-12-05 10:42:47 +0000

    Day 5 - Initial ugly solution.

--- Day04/Program.cs
+++ Day04/Program.cs
@@ -1,8 +1,8 @@
-﻿using Day04;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 var lines = File.ReadAllLines("../../../input.txt");
 
@@ -42,6 +42,25 @@ foreach(var line in lines)
     }
 }
 
-Console.WriteLine(passports.Count(s => s.IsValid()));
+Console.WriteLine(passports.Count(s => IsValidPassport(s)));
 
+static bool IsValidPassport(Passport passport) => int.TryParse(passport.BirthYear, out int birthYear) && birthYear >= 1920 && birthYear <= 2002 &&
+                                                  int.TryParse(passport.IssueYear, out int issueYear) && issueYear >= 2010 && issueYear <= 2020 &&
+                                                  int.TryParse(passport.ExpirationYear, out int expirationYear) && expirationYear >= 2020 && expirationYear <= 2030 &&
+                                                  Regex.IsMatch(passport.Height ?? "", "(1(([5-8][0-9])|(9[0-3]))cm)|(((59)|(6[0-9])|(7[0-6]))in)") &&
+                                                  Regex.IsMatch(passport.HairColour ?? "", "#[\\da-f]{6}") &&
+                                                  Regex.IsMatch(passport.EyeColour ?? "", "amb|blu|brn|gry|grn|hzl|oth") &&
+                                                  Regex.IsMatch(passport.PassportID ?? "", "^[\\d]{9}$");
+
+class Passport
+{
+    public string BirthYear { get; set; }
+    public string IssueYear { get; set; }
+    public string ExpirationYear { get; set; }
+    public string Height { get; set; }
+    public string HairColour { get; set; }
+    public string EyeColour { get; set; }
+    public string PassportID { get; set; }
+    public string CountryID { get; set; }
+}
 
---
+++ Day05/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+var lines = File.ReadAllLines("../../../input.txt");
+
+Dictionary<int, List<int>> seats = new();
+
+int highestSeatId = 0;
+foreach (var line in lines)
+{
+    int row = BinarySpacePartition(line.Substring(0, 7), 0, 127);
+    int column = BinarySpacePartition(line.Substring(7, 3), 0, 7);
+
+    if(row != 127 && row != 0)
+    {
+        var columns = seats.GetValueOrDefault(row);
+        if (columns == null)
+            columns = new List<int>();
+
+        columns.Add(column);
+        seats[row] = columns;
+    }
+
+    int seatId = row * 8 + column;
+
+    if (seatId > highestSeatId)
+        highestSeatId = seatId;
+}
+
+HashSet<int> lookup = new() { 0, 1, 2, 3, 4, 5, 6, 7 };
+foreach(var key in seats.Keys)
+{
+    HashSet<int> values = new(seats[key]);
+    values.SymmetricExceptWith(lookup);
+
+    if (values.Count == 1)
+    {
+        Console.WriteLine(key);
+        Console.WriteLine(values.ToList()[0]);
+        Console.WriteLine($"Your seat id: {key * 8 + values.ToList()[0]}");
+    }
+}
+
+static int BinarySpacePartition(string commands, int min, int max)
+{
+    Console.WriteLine(commands);
+    int result = 0;
+    foreach(char letter in commands)
+    {
+        if (min == max) return min;
+
+        if (letter == 'F' || letter == 'L')
+        {
+            max -= (max - min) / 2 + 1;
+        }
+        else
+        {
+            min += (max - min) / 2 + 1;
+        }
+
+        result = min;
+    }
+    return result; 
+}
