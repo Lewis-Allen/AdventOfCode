529f0a9ab13131e26b335d11c70a4befcc8c435e
Lewis-Allen <lewisn11@hotmail.co.uk>
2020-12-06 10:18:37 +0000

    Initial day 6. Need to do some LINQ magic on this one..

---
+++ Day06/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+var lines = File.ReadAllText("../../../input.txt")
+    .Split("\r\n\r\n", StringSplitOptions.RemoveEmptyEntries)
+    .Select(s => s.Replace("\r\n", ""))
+    .ToList();
+
+Console.WriteLine(lines.Sum(s => s.Distinct().Count()));
+
+lines = File.ReadAllText("../../../input.txt")
+    .Split("\r\n\r\n", StringSplitOptions.RemoveEmptyEntries)
+    .ToList();
+
+int count = 0;
+foreach(var group in lines)
+{
+    IEnumerable<char> letters = "abcdefghijklmnopqrstuvwxyz";
+    
+    var people = group.Split("\r\n");
+
+    for (var i = 0; i < people.Length; i++)
+    {
+        letters = letters.Intersect(people[i]);
+    }
+
+    count += letters.Count();
+}
+
+Console.WriteLine(count);
+
+
