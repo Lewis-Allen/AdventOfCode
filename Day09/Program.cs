e97d582f259fee5b49ac16d5ad4e66cf0a0faa8c
Lewis-Allen <lewisn11@hotmail.co.uk>
2020-12-09 19:53:45 +0000

    Very tired tonight but here's day 09 initial grim solution.

---
+++ Day09/Program.cs
@@ -0,0 +1,39 @@
+﻿using Combinatorics.Collections;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+var lines = File.ReadAllLines("../../../input.txt");
+
+for(var i = 25; i < lines.Length; i++)
+{
+    List<long> preamble = new();
+
+    for(int j = i - 25; j < i; j++)
+    {
+        preamble.Add(long.Parse(lines[j]));
+    }
+    long result = long.Parse(lines[i]);
+
+    Combinations<long> combos = new Combinations<long>(preamble, 2);
+    if(!combos.Any(s => s.Sum() == result))
+    {
+        Console.WriteLine(result);
+        break;
+    }
+}
+
+long target = 1398413738L;
+
+for(int count = 2; count < lines.Length; count++)
+    for (int i = 0; i < (lines.Length - count); i++)
+    {
+        var elements = lines.Skip(i).Take(count).Select(long.Parse);
+        long segSum = elements.Sum();
+
+        if (segSum == target)
+            Console.WriteLine(elements.Min() + elements.Max());
+    }
+
+
