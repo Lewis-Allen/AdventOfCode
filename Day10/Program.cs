2b0d8161838474e706df12563f80de803841a657
Lewis-Allen <lewisn11@hotmail.co.uk>
2020-12-10 22:07:15 +0000

    TRIBONACCI AND MEMOIZATION

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -25,7 +25,30 @@ for(var i = 0; i < lines.Count; i++)
     a = b;
 }
 
-// Add my own adaptor...
+// Add my own adapter...
 threes++;
 
 Console.WriteLine($"Ones ({ones}) by Threes ({threes}) = {ones * threes}");
+
+// Part Two
+long[] memo = new long[lines.Count];
+memo[0] = 1;
+memo[1] = 2;
+memo[2] = 4;
+
+for (var i = 3; i < lines.Count; i++)
+{
+    var threshold = lines[i] - 3;
+    memo[i] = 0;
+
+    if (lines[i - 3] >= threshold)
+        memo[i] += memo[i - 3];
+
+    if (lines[i - 2] >= threshold)
+        memo[i] += memo[i - 2];
+
+    if (lines[i - 1] >= threshold)
+        memo[i] += memo[i - 1];
+}
+
+Console.WriteLine(memo[lines.Count - 1]);
