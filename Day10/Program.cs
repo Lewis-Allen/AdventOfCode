24e8ddfa069d71506c2579356f8523ad0412443e
Lewis-Allen <lewisn11@hotmail.co.uk>
2020-12-10 18:43:32 +0000

    Day 10 Part 1 unrefined.

---
+++ Day10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+var lines = Array.ConvertAll(File.ReadAllLines("../../../input.txt"), int.Parse).ToList();
+
+lines.Sort();
+
+int ones = 0;
+int threes = 0;
+
+var a = 0;
+
+for(var i = 0; i < lines.Count; i++)
+{
+    var b = lines[i];
+    int difference = b - a;
+
+    if (difference == 1)
+        ones++;
+
+    if (difference == 3)
+        threes++;
+
+    a = b;
+}
+
+// Add my own adaptor...
+threes++;
+
+Console.WriteLine($"Ones ({ones}) by Threes ({threes}) = {ones * threes}");
