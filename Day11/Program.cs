3014f222c502909bb0e041f8d9bce794ed8fbb2d
Lewis-Allen <lewisn11@hotmail.co.uk>
2020-12-11 19:31:55 +0000

    Day 11 - This is a disgusting solution that needs refactoring but it works!

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -4,13 +4,12 @@ using System.IO;
 using System.Linq;
 using System.Text;
 
-var lines = File.ReadAllLines("../../../example1.txt");
+var lines = File.ReadAllLines("../../../input.txt");
 
 var current = new List<string>(lines);
 var next = new List<string>(lines);
 
 var changed = false;
-PrintLines(current);
 do
 {
     changed = false;
@@ -20,7 +19,7 @@ do
         {
             var character = current[y][x];
 
-            if (character == 'L' && CheckSurroundingsPartOne(current, y, x) == 0)
+            if (character == 'L' && CheckSurroundingsPartTwo(current, y, x) == 0)
             {
                 StringBuilder str = new(next[y]);
                 str[x] = '#';
@@ -28,7 +27,7 @@ do
                 changed = true;
             }
 
-            if(character == '#' && CheckSurroundingsPartOne(current, y, x) >= 4)
+            if(character == '#' && CheckSurroundingsPartTwo(current, y, x) >= 5)
             {
                 StringBuilder str = new(next[y]);
                 str[x] = 'L';
@@ -39,7 +38,6 @@ do
     }
 
     current = new List<string>(next);
-    PrintLines(current);
 } while (changed);
 
 var count = current.Sum(s => s.Count(c => c == '#'));
@@ -65,25 +63,126 @@ static int CheckSurroundingsPartTwo(List<string> lines, int y, int x)
 {
     int noOfOccupied = 0;
 
-    // Dont count current seat
-    int currentOccupied = lines[y][x] == '#' ? 1 : 0;
+    // Left
+    for(int i = x - 1; i >= 0; i--)
+    {
+        if (lines[y][i] == 'L')
+            break;
 
-    var occupied = lines.Where((a, i) => i <= y + 1 && i >= y - 1)
-        .SelectMany(s => s.Where((b, j) => j <= x + 1 && j >= x - 1))
-        .Count(s => s == '#');
+        if (lines[y][i] == '#')
+        {
+            noOfOccupied++;
+            break;
+        }
+    }
 
-    noOfOccupied = occupied - currentOccupied;
+
+    // Right
+    for (int i = x + 1; i < lines[y].Length; i++)
+    {
+        if (lines[y][i] == 'L')
+            break;
+
+        if (lines[y][i] == '#')
+        {
+            noOfOccupied++;
+            break;
+        }
+    }
+
+    // Up
+    for (int i = y - 1; i >= 0; i--)
+    {
+        if (lines[i][x] == 'L')
+            break;
+
+        if (lines[i][x] == '#')
+        {
+            noOfOccupied++;
+            break;
+        }
+    }
+
+    // Down
+    for (int i = y + 1; i < lines.Count; i++)
+    {
+        if (lines[i][x] == 'L')
+            break;
+
+        if (lines[i][x] == '#')
+        {
+            noOfOccupied++;
+            break;
+        }
+    }
+
+    // Upper Left
+    for(int i = 1;; i++)
+    {
+        if (x - i < 0 || y - i < 0)
+            break;
+
+        if(lines[y - i][x - i] == 'L')
+        {
+            break;
+        }
+        if (lines[y - i][x - i] == '#')
+        {
+            noOfOccupied++;
+            break;
+        }
+    }
+
+    // Upper Right
+    for(int i = 1; ; i++)
+    {
+        if (y - i < 0 || x + i >= lines[y].Length)
+            break;
+
+        if (lines[y - i][x + i] == 'L')
+        {
+            break;
+        }
+        if (lines[y - i][x + i] == '#')
+        {
+            noOfOccupied++;
+            break;
+        }
+    }
+
+    // Lower Right
+    for (int i = 1; ; i++)
+    {
+        if (y + i >= lines.Count || x + i >= lines[y].Length)
+            break;
+
+        if (lines[y + i][x + i] == 'L')
+        {
+            break;
+        }
+        if (lines[y + i][x + i] == '#')
+        {
+            noOfOccupied++;
+            break;
+        }
+    }
+
+    // Lower Left
+    for(int i = 1; ; i++)
+    {
+        if (y + i >= lines.Count || x - i < 0)
+            break;
+
+        if (lines[y + i][x - i] == 'L')
+        {
+            break;
+        }
+        if (lines[y + i][x - i] == '#')
+        {
+            noOfOccupied++;
+            break;
+        }
+    }
 
     return noOfOccupied;
-}
-
-static void PrintLines(List<string> lines)
-{
-    foreach(var line in lines)
-    {
-        Console.WriteLine(line);
-    }
-    Console.WriteLine();
-    Console.WriteLine("----------------------------------------------------------------------------------------------------");
-    Console.WriteLine();
 }
