b69178380cc32a9b7dc0d5f57ed5d958c81511ee
Lewis-Allen <lewisn11@hotmail.co.uk>
2020-12-13 16:33:03 +0000

    There is something wrong with my part two but I can't figure out what it is...

--- Day13/Program.cs
+++ Day13/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
 var lines = File.ReadAllLines("../../../Example1.txt");
 
-PartOne(lines);
+//PartOne(lines);
+
+PartTwo(lines);
 
 static void PartOne(string[] lines)
 {
@@ -25,6 +28,43 @@ static void PartOne(string[] lines)
 static void PartTwo(string[] lines)
 {
     var busIDs = lines[1].Split(',');
+    var totals = new List<long>();
 
-    Console.WriteLine($"Part Two: ");
+    var filteredBusIDs = busIDs
+            .Select((value, index) => (value, index))
+            .Where(s => s.value != "x")
+            .Select(s => (long.Parse(s.value), s.index))
+            .ToList();
+
+    // Sum of all values
+    long N = filteredBusIDs.Aggregate(1L, (acc, s) => acc * s.Item1);
+
+    for (int i = 0; i < busIDs.Length; i++)
+    {
+        if (!long.TryParse(busIDs[i], out long busID))
+            continue;
+
+        long Bi = i;
+        long Ni = N / busID;
+        long Xi = ModuloInverse(Ni, busID);
+
+        Console.WriteLine($"Current Index: {i}");
+        Console.WriteLine($"N: {N}, BusID: {busID}, Ni: {N / busID}");
+        Console.WriteLine($"ModuloInverse({Ni},{busID}) = {Xi}");
+
+        totals.Add(Bi * Ni * Xi);
+    }
+
+    long timestamp = totals.Sum() % N;
+
+    Console.WriteLine($"Part Two: The timestamp we are looking for is {timestamp}.");
+}
+
+static long ModuloInverse(long a, long m)
+{
+    a %= m;
+    for (int x = 1; x < m; x++)
+        if ((a * x) % m == 1)
+            return x;
+    return 1;
 }
