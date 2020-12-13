7173ade8d8efffab19a5f4550aa8af764b8f3fb5
Lewis-Allen <lewisn11@hotmail.co.uk>
2020-12-13 20:11:11 +0000

    Add a short LINQy version.

--- Day13/Program.cs
+++ Day13/Program.cs
@@ -9,6 +9,8 @@ PartOne(lines);
 
 PartTwo(lines);
 
+PartTwoCondensed(lines);
+
 static void PartOne(string[] lines)
 {
     var leavingTime = int.Parse(lines[0]);
@@ -22,22 +24,33 @@ static void PartOne(string[] lines)
 
     var minutesToWait = theBus.Item2 - leavingTime;
 
-    Console.WriteLine($"Part One: The first bus that leaves after {leavingTime} is Bus ID {theBus.s} at {theBus.Item2}. There is a {minutesToWait} minute waiting time. Waiting time multiplied by bus ID is {theBus.s * minutesToWait}.");
+    Console.WriteLine($"Part One - The first bus that leaves after {leavingTime} is Bus ID {theBus.s} at {theBus.Item2}. There is a {minutesToWait} minute waiting time. Waiting time multiplied by bus ID is {theBus.s * minutesToWait}.");
 }
 
+/**
+ * This can be done in a couple of LINQ statements (See PartTwoCondensed).
+ * But if I look back on this code one day, i'll probably have no idea what I was doing.
+ * So i'm keeping this one because it's more readable.
+ */
 static void PartTwo(string[] lines)
 {
-    var busIDs = lines[1].Split(',');
     var totals = new List<long>();
 
-    var filteredBusIDs = busIDs
-            .Select((value, index) => (value, index))
-            .Where(s => s.value != "x")
-            .Select(s => (long.Parse(s.value), s.index))
-            .ToList();
+    var filteredBusIDs = lines[1]
+        .Split(',')
+        .Select((value, index) => (value, index))
+        .Where(s => s.value != "x")
+        .Select(s => (long.Parse(s.value), s.index))
+        .ToList();
 
     // Product of all values
     long N = filteredBusIDs.Aggregate(1L, (acc, s) => acc * s.Item1);
+
+    var test = filteredBusIDs
+        .Select(s => (s.Item1 - s.index) * (N / s.Item1) * ModuloInverse(N / s.Item1, s.Item1))
+        .Sum();
+
+    Console.WriteLine(test % N);
 
     for (int i = 0; i < filteredBusIDs.Count; i++)
     {
@@ -52,7 +65,29 @@ static void PartTwo(string[] lines)
 
     long timestamp = totals.Sum() % N;
 
-    Console.WriteLine($"Part Two: The timestamp we are looking for is {timestamp}.");
+    Console.WriteLine($"Part Two - The timestamp we are looking for is {timestamp}.");
+}
+
+/**
+ * Short version of the above PartTwo function.
+ * But it's a lot less readable.
+ */
+static void PartTwoCondensed(string[] lines)
+{
+    var filteredBusIDs = lines[1]
+        .Split(',')
+        .Select((value, index) => (value, index))
+        .Where(s => s.value != "x")
+        .Select(s => (long.Parse(s.value), s.index))
+        .ToList();
+
+    long N = filteredBusIDs.Aggregate(1L, (acc, s) => acc * s.Item1);
+
+    var total = filteredBusIDs
+        .Select(s => (s.Item1 - s.index) * (N / s.Item1) * ModuloInverse(N / s.Item1, s.Item1))
+        .Sum();
+
+    Console.WriteLine($"Part Two Condensed - The timestamp we are looking for is {total % N}.");
 }
 
 static long ModuloInverse(long a, long m)
