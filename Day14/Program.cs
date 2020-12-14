63cef4b04d747b0f1ba8b777acd1cf049eb28531
Lewis-Allen <lewisn11@hotmail.co.uk>
2020-12-14 18:32:56 +0000

    Switching PCs...

--- Day14/Program.cs
+++ Day14/Program.cs
@@ -3,9 +3,11 @@ using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 
-var lines = File.ReadAllLines("../../../Input.txt");
+var lines = File.ReadAllLines("../../../Example2.txt");
 
 PartOne(lines);
+
+PartTwo(lines);
 
 static void PartOne(string[] lines)
 {
@@ -21,9 +23,6 @@ static void PartOne(string[] lines)
             long index = long.Parse(nums[0].Value);
             long value = long.Parse(nums[1].Value);
 
-            Console.WriteLine($"Value: {Convert.ToString(value, 2)}.");
-            Console.WriteLine($"Mask : {currentMask}.");
-
             for (var i = 0; i < currentMask.Length; i++)
             {
                 char letter = currentMask[currentMask.Length - 1 - i];
@@ -34,7 +33,47 @@ static void PartOne(string[] lines)
                 if(letter == '0')
                     value &= ~(1L << i);
             }
-            Console.WriteLine($"Value: {Convert.ToString(value, 2)}.");
+            memory[index] = value;
+        }
+        else
+        {
+            // Set the mask
+            currentMask = nums[0].Value;
+        }
+    }
+
+    Console.WriteLine($"The sum of all values in memory is {memory.Sum()}.");
+}
+
+static void PartTwo(string[] lines)
+{
+    long[] memory = new long[100000];
+    string currentMask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+
+    foreach (var line in lines)
+    {
+        var nums = Regex.Matches(line, "[\\dX]+");
+
+        if (nums.Count > 1)
+        {
+            long index = long.Parse(nums[0].Value);
+            long value = long.Parse(nums[1].Value);
+
+            for (var i = 0; i < currentMask.Length; i++)
+            {
+                char letter = currentMask[currentMask.Length - 1 - i];
+
+                if (letter == '1')
+                    value |= 1L << i;
+
+                if (letter == '0')
+                    value &= ~(1L << i);
+
+                if (letter == 'X')
+                {
+
+                }
+            }
             memory[index] = value;
         }
         else
