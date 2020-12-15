8e92e4e364134e5ebaf7a6f121b27dd2a0c46f50
Lewis-Allen <lewisn11@hotmail.co.uk>
2020-12-15 21:42:51 +0000

    Day 15 Part One - Not so efficient.

---
+++ Day15/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+var lines = File.ReadAllLines("../../../Input.txt").Select(s => s.Split(",")).ToArray();
+
+foreach (var line in lines)
+{
+    Console.WriteLine(GetNthPosition(Array.ConvertAll(line, long.Parse), 2020));
+}
+
+static long GetNthPosition(long[] numbers, long pos)
+{
+    List<long> numbersList = numbers.ToList();
+    int turnCounter = numbersList.Count;
+
+    do
+    {
+        
+        long number = numbersList.Last();
+        if (numbersList.Count(c => c == number) < 2)
+        {
+            numbersList.Add(0);
+        }
+        else
+        {
+            long lastOccurence = Array.LastIndexOf(numbersList.SkipLast(1).ToArray(), number) + 1;
+
+            numbersList.Add(turnCounter - lastOccurence);
+        }
+
+        turnCounter++;
+    } while (numbersList.Count < pos);
+
+    return numbersList.Last();
+}
