228ad8f940338060127bb689556499fa6a513aac
Lewis-Allen <lewisn11@hotmail.co.uk>
2020-12-17 19:06:21 +0000

    Day 17 Adapted for Part 2. This took like 2 minutes to run lol.

--- Day17/Program.cs
+++ Day17/Program.cs
@@ -13,7 +13,7 @@ for(int y = 0; y < lines.Length; y++)
 {
     for(int x = 0; x < lines[y].Length; x++)
     {
-        cubes.Add(new(x, y, 0, lines[y][x] == '#'));
+        cubes.Add(new(x, y, 0, 0, lines[y][x] == '#'));
     }
 }
 
@@ -23,31 +23,32 @@ for (int c = 0; c < noOfCycles; c++)
     int minX = cubes.Min(c => c.X);
     int minY = cubes.Min(c => c.Y);
     int minZ = cubes.Min(c => c.Z);
+    int minW = cubes.Min(c => c.W);
 
     int maxX = cubes.Max(c => c.X);
     int maxY = cubes.Max(c => c.Y);
     int maxZ = cubes.Max(c => c.Z);
+    int maxW = cubes.Max(c => c.W);
 
     HashSet<Cube> newCubes = new();
 
     for(int z = minZ - 1; z <= maxZ + 1; z++)
     {
-        for(int y = minY - 1; y <= maxY + 1; y++)
+        for (int w = minW - 1; w <= maxW + 1; w++)
         {
-            for(int x = minX - 1; x <= maxX + 1; x++)
+            for (int y = minY - 1; y <= maxY + 1; y++)
             {
-                Cube cube = cubes.FirstOrDefault(c => c.X == x && c.Y == y && c.Z == z);
-                
-                bool currentlyActive = cube != null && cube.Active;
+                for (int x = minX - 1; x <= maxX + 1; x++)
+                {
+                    Cube cube = cubes.FirstOrDefault(c => c.X == x && c.Y == y && c.Z == z && c.W == w);
 
-                Cube newCube = new(x, y, z, GoActive(x, y, z, currentlyActive, cubes));
-                newCubes.Add(newCube);
-                Console.Write(newCube.Active ? '#' : '.');
+                    bool currentlyActive = cube != null && cube.Active;
+
+                    Cube newCube = new(x, y, z, w, GoActive(x, y, z, w, currentlyActive, cubes));
+                    newCubes.Add(newCube);
+                }
             }
-            Console.WriteLine();
         }
-
-        Console.WriteLine("---------------------");
     }
 
     cubes = newCubes;
@@ -55,20 +56,22 @@ for (int c = 0; c < noOfCycles; c++)
 
 Console.WriteLine(cubes.Count(c => c.Active));
 
-static bool GoActive(int x, int y, int z, bool active, HashSet<Cube> cubes)
+static bool GoActive(int x, int y, int z, int w, bool active, HashSet<Cube> cubes)
 {
     int nearbyActives = cubes.Where(c =>
     {
-        return c.Active && !(c.X == x && c.Y == y && c.Z == z) &&
+        return c.Active && !(c.X == x && c.Y == y && c.Z == z && c.W == w) &&
                c.X >= x - 1 &&
                c.X <= x + 1 &&
                c.Y >= y - 1 &&
                c.Y <= y + 1 &&
                c.Z >= z - 1 &&
-               c.Z <= z + 1;
+               c.Z <= z + 1 &&
+               c.W >= w - 1 &&
+               c.W <= w + 1;
     }).Count();
 
     return (active && (nearbyActives == 2 || nearbyActives == 3)) || (!active && nearbyActives == 3);
 }
 
-public record Cube(int X, int Y, int Z, bool Active);
+public record Cube(int X, int Y, int Z, int W, bool Active);
