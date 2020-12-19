7790cd643affaf1bc4136cd13b48a21437cc3bd6
Lewis-Allen <lewisn11@hotmail.co.uk>
2020-12-19 19:09:04 +0000

    Day 19 Part 1

---
+++ Day19/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+var lines = File.ReadAllText("../../../Input2.txt").Split("\r\n\r\n");
+
+var rules = lines[0].Split("\r\n");
+var messages = lines[1].Split("\r\n");
+
+rules = rules.OrderBy(r => int.Parse(Regex.Match(r, "\\d+").Value)).ToArray();
+
+string regexString = " " + Regex.Match(rules[0], "(?<=(: )).*").Value + " ";
+
+while(Regex.IsMatch(regexString, "\\d+"))
+{
+    foreach(var match in Regex.Matches(regexString, "[^\\d]\\d+[^\\d]").ToList())
+    {
+        var ruleNo = int.Parse(match.Value);
+        var index = rules.ToList().FindIndex(s => Regex.IsMatch(s, $"^{ruleNo}:"));
+
+        var replacement = " ( " + Regex.Match(rules[index], "(?<=(: )).*").Value + " ) ";
+
+        regexString = regexString.Replace(match.Value, replacement);
+    }
+    Console.WriteLine(regexString);
+}
+
+regexString = regexString.Replace(" ", "").Replace("\"", "");
+
+var count = messages.Count(m =>
+{
+    return Regex.IsMatch(m, regexString) &&
+           Regex.Match(m, regexString).Value.Length == m.Length;
+});
+
+// Part One
+Console.WriteLine($"Part One Matching Rules = {count}.");
