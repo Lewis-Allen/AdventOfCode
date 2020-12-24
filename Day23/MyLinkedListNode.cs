5fe65fdcb2a68b9c753a5f953885108aef525f53
Lewis-Allen <lewisn11@hotmail.co.uk>
2020-12-24 00:37:43 +0000

    Finally finished day 23.. It's terrible and needs refactoring but my brain is frazzled.

---
+++ Day23/MyLinkedListNode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day23
+{
+    /// <summary>
+    /// The standard LinkedListNode library was giving me trouble..
+    /// I wanted to easily destroy and link elements so I just made a bog-standard class here.
+    /// </summary>
+    public class MyLinkedListNode<T>
+    {
+        public MyLinkedListNode<T> Next { get; set; }
+
+        public T Value { get; set; }
+
+        public MyLinkedListNode(T value)
+        {
+            Value = value;
+        }
+    }
+}
--- Day23/Program.cs
+++ Day23/Program.cs
@@ -1,25 +1,27 @@
-﻿using System;
+﻿using C5;
+using Day23;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 
-List<int> input = File.ReadAllText("../../../Input.txt").ToCharArray().Select(n => n - '0').ToList();
+List<long> input = File.ReadAllText("../../../Input.txt").ToCharArray().Select(n => (long)(n - '0')).ToList();
 
-List<int> numbers = new(input);
-int currentCup = numbers[0];
+List<long> numbers = new(input);
+long currentCup = numbers[0];
 
-int movesCompleted = 0;
+long movesCompleted = 0;
 
 while(movesCompleted < 100)
 {
     var currentIndex = numbers.FindIndex(c => c == currentCup);
 
-    List<int> removedCups = new();
+    List<long> removedCups = new();
     for(int i = 0; i < 3; i++)
     {
-        var removeIndex = (currentIndex + 1) % numbers.Count;
-        var number = numbers[removeIndex];
+        int removeIndex = (currentIndex + 1) % numbers.Count;
+        long number = numbers[removeIndex];
         removedCups.Add(number);
         numbers.RemoveAt(removeIndex);
         currentIndex = numbers.FindIndex(c => c == currentCup);
@@ -32,6 +34,7 @@ while(movesCompleted < 100)
     currentCup = numbers[(numbers.FindIndex(c => c == currentCup) + 1) % numbers.Count];
 
     movesCompleted++;
+    Console.WriteLine(movesCompleted);
 }
 
 var startPos = numbers.IndexOf(1);
@@ -41,3 +44,89 @@ for(int i = startPos; i < startPos + 8; i++)
     sb.Append(numbers[(i + 1) % numbers.Count]);
 }
 Console.WriteLine(string.Join("", sb.ToString()));
+
+// Part Two
+const long MAX_ELEMENTS = 1000000;
+const long NO_OF_MOVES = 10000000;
+
+TreeDictionary<long, MyLinkedListNode<long>> NodeLocations = new();
+
+MyLinkedListNode<long> head = new(input[0]);
+NodeLocations.Add(head.Value, head);
+
+var current = head;
+
+for (int i = 1; i < input.Count; i++)
+{
+    current.Next = new (input[i]);
+    NodeLocations.Add(current.Next.Value, current.Next);
+    current = current.Next;
+}
+
+for (long i = 10; i <= MAX_ELEMENTS; i++)
+{
+    MyLinkedListNode<long> newNode = new(i);
+    if(i == 10)
+    {
+        NodeLocations[input[8]].Next = newNode;
+    }
+    else
+    {
+        NodeLocations[i - 1].Next = newNode;
+    }
+    NodeLocations[i] = newNode;
+}
+NodeLocations[MAX_ELEMENTS].Next = head;
+
+var currentNode = head;
+movesCompleted = 0;
+while(movesCompleted < NO_OF_MOVES)
+{
+    List<long> nextThree = new()
+    {
+        currentNode.Next.Value,
+        currentNode.Next.Next.Value,
+        currentNode.Next.Next.Next.Value
+    };
+
+    long? predecessorValue = null;
+    long valueToCheck = currentNode.Value;
+
+    while(predecessorValue == null)
+    {
+        if(NodeLocations.TryPredecessor(valueToCheck, out var predecessor))
+        {
+            var value = predecessor.Value.Value;
+            if(nextThree.Contains(predecessor.Value.Value))
+            {
+                valueToCheck = value;
+            }
+            else
+            {
+                predecessorValue = value;
+            }
+        }
+        else
+        {
+            valueToCheck = MAX_ELEMENTS + 1;
+        }
+    }
+
+    MyLinkedListNode<long> destination = NodeLocations[(long)predecessorValue];
+
+    long? currentNodeNext = destination.Next?.Value;
+    long? destNext = currentNode.Next.Next.Next.Next?.Value;
+    long? currentNodeNextNextNextNext = currentNode.Next?.Value;
+
+    destination.Next = currentNodeNextNextNextNext is null ? null : NodeLocations[(long)currentNodeNextNextNextNext];
+    destination.Next.Next.Next.Next = currentNodeNext is null ? null : NodeLocations[(long)currentNodeNext];
+    currentNode.Next = destNext is null ? null : NodeLocations[(long)destNext];
+
+    currentNode = currentNode.Next;
+
+    movesCompleted++;
+}
+
+var final = NodeLocations[1];
+long res = final.Next.Value * final.Next.Next.Value;
+Console.WriteLine(res);
