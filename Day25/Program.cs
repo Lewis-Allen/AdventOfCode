5272bc3a82874b1d6f9053238c0b7ebb754e5a2b
Lewis-Allen <lewisn11@hotmail.co.uk>
2020-12-25 19:19:14 +0000

    Solution to day 25. Advent of Code 2020 is done! Might go back and refactor some solutions though we'll see.

---
+++ Day25/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+var lines = File.ReadAllLines("../../../Input.txt");
+
+var cardKey = lines[0];
+var doorKey = lines[1];
+
+var cardLoopSize = FindLoopSize(long.Parse(cardKey), 7);
+
+var res = Transform(long.Parse(doorKey), cardLoopSize);
+
+Console.WriteLine(res);
+
+static long FindLoopSize(long key, long subjectNumber)
+{
+    long value = 1;
+    int loopSize = 0;
+    while(value != key)
+    {
+        value *= subjectNumber;
+        value %= 20201227;
+
+        loopSize++;
+    }
+
+    return loopSize;
+}
+
+static long Transform(long subjectNumber, long loopSize)
+{
+    long value = 1;
+    for(int i = 0; i < loopSize; i++)
+    {
+        value *= subjectNumber;
+        value %= 20201227;
+    }
+
+    return value;
+}
