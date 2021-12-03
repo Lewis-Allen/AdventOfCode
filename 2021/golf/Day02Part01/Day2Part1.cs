e190079c80fe921769b25dbc559cc06678eca845
Lewis-Allen <lewisn11@hotmail.co.uk>
2021-12-03 01:15:40 +0000

    2021 - Day 02 - Part 1 - Golf

---
+++ 2021/golf/Day02Part01/Day2Part1.cs
@@ -0,0 +1,4 @@
+﻿var i=File.ReadAllLines("2")
+    .ToArray();
+
+Console.WriteLine(i.Sum(a=>a[0]=='f'?a[^1]-'0':0)*i.Sum(a=>a[0]=='d'?a[^1]-'0':a[0]=='u'?-(a[^1]-'0'):0));
---
+++ 2021/golf/Day02Part02/Day2Part2.cs
@@ -0,0 +1,5 @@
+﻿// Not done yet...
+var i = File.ReadAllLines("2")
+    .ToArray();
+
+Console.WriteLine(i.Sum(a => a[0] == 'f' ? a[^1] - '0' : 0) * i.Sum(a => a[0] == 'd' ? a[^1] - '0' : a[0] == 'u' ? -(a[^1] - '0') : 0));
