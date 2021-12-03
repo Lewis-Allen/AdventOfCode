b73f76e2ec1a09e52f37b7c337fa4b9cfda545b1
Lewis-Allen <lewisn11@hotmail.co.uk>
2021-12-03 23:31:19 +0000

    2021 - Day 03 - Golf

---
+++ 2021/golf/Day03Part01/Day3Part1.cs
@@ -0,0 +1,14 @@
+﻿var f=File.ReadLines("3")
+    .ToList();
+
+int g,e,i,a;
+for(g=e=i=0;i<f[0].Length;i++)
+{
+    var c=f.Count(b=>b[i]==48)>f.Count/2;
+
+    a=c?1:0;
+    g=(g<<1)+a^1;
+    e=(e<<1)+a;
+}
+
+Console.Write(g*e);
