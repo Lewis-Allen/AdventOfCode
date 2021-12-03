3a40c25ff66c87d6049c28276192ffec665f9a8a
Lewis-Allen <lewisn11@hotmail.co.uk>
2021-12-03 00:45:22 +0000

    2021 - Day 01 - Golf

---
+++ 2021/golf/Day01Part01/Day1Part1.cs
@@ -0,0 +1,4 @@
+﻿var i = Array.ConvertAll(File.ReadAllLines("1"), int.Parse);
+
+Console.Write(Enumerable.Range(1, i.Length - 1)
+    .Count(a => i[a] > i[a - 1]));
---
+++ 2021/golf/Day01Part02/Day1Part2.cs
@@ -0,0 +1,4 @@
+﻿var i=Array.ConvertAll(File.ReadAllLines("1"),int.Parse);
+
+Console.Write(Enumerable.Range(3,i.Length-3)
+    .Count(a=>i[a]+i[a-1]+i[a-2]>i[a-1]+i[a-2]+i[a-3]));
