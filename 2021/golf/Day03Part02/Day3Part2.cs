02e15d447cf9f5cbb0233abb046cf7e9ab2ba4e6
Lewis-Allen <lewisn11@hotmail.co.uk>
2021-12-04 00:43:47 +0000

    2021 - Day 3 Part 2 - Golf Optimization

--- 2021/golf/Day03Part02/Day3Part2.cs
+++ 2021/golf/Day03Part02/Day3Part2.cs
@@ -1,12 +1,10 @@
 ﻿var f=File.ReadLines("3");
 
-int s(string[]b,int i)=>b.Count(c=>c[i]<49)>b.Length/2?1:0;
-
 int r(string[]b,bool k)
 {
     for(int j=0;b.Length>1;j++)
-        b=b.Where(o=>k==(o[j]==49-s(b,j))).ToArray();
+        b=b.Where(o=>k==(o[j]<49==(b.Count(c=>c[j]<49)>b.Length/2))).ToArray();
 
     return Convert.ToInt32(b[0],2);
 }
-Console.WriteLine(r(f.ToArray(),1>0)*r(f.ToArray(),0>1));
+Console.Write(r(f.ToArray(),1>0)*r(f.ToArray(),0>1));
