33d7ecca53f5f21b69ab4e2532eff96b760c0e40
Lewis-Allen <lewisn11@hotmail.co.uk>
2021-12-06 18:57:17 +0000

    2021 - Day 06

--- 2021/src/Day06/LanternfishCalculator.cs
+++ 2021/src/Day06/LanternfishCalculator.cs
@@ -6,22 +6,24 @@ public class LanternfishCalculator
 {
     public static long GetFishCountAfterXDays(int[] initialState, int days)
     {
+        // Initial setup
         long[] birthCycles = new long[9];
 
         foreach (var cycle in initialState)
             birthCycles[cycle]++;
 
+        // Begin simulating days
         for(int i = 0; i < days; i++)
         {
-            long temp = birthCycles[0];
+            long births = birthCycles[0];
 
             for(int j = 0; j < 8; j++)
             {
                 birthCycles[j] = birthCycles[j + 1];
             }
 
-            birthCycles[6] += temp;
-            birthCycles[8] = temp;
+            birthCycles[6] += births;
+            birthCycles[8] = births;
         }
 
         return birthCycles.Sum();
